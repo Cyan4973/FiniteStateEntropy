3a175ae917a22fc4d20456f68489a24e2f98201a
Yann Collet <cyan@fb.com>
2017-06-15 16:12:34 -0700

    updated fuzzerU16 to generate skewed distribution

--- programs/fuzzerU16.c
+++ programs/fuzzerU16.c
@@ -82,7 +82,7 @@ typedef unsigned long long  U64;
 ***************************************************/
 #define DISPLAY(...)         fprintf(stderr, __VA_ARGS__)
 #define DISPLAYLEVEL(l, ...) if (displayLevel>=l) { DISPLAY(__VA_ARGS__); }
-static unsigned displayLevel = 2;   // 0 : no display  // 1: errors  // 2 : + result + interaction + warnings ;  // 3 : + progression;  // 4 : + information
+static unsigned displayLevel = 2;   /* 0: no display;  1: errors;  2: + result + interaction + warnings;  3: + progression;  4: + information */
 
 
 /*-*************************************************
@@ -114,14 +114,15 @@ static unsigned FUZ_rand (unsigned* src)
 }
 
 
-static void generateU16 (U16* buffer, size_t buffSize, double p, U32 seedSrc)
+static void generateU16 (U16* buffer, size_t buffSize,
+                         U16 start, double p, U32 seedSrc)
 {
     U16 tableU16[PROBATABLESIZE];
     U32 remaining = PROBATABLESIZE;
     U32 pos = 0;
     U16* op = buffer;
     U16* const oend = op + buffSize;
-    U16 val16 = 240;
+    U16 val16 = start;
     U16 max16 = FSE_MAX_SYMBOL_VALUE;
     U32 seed = seedSrc;
 
@@ -137,36 +138,46 @@ static void generateU16 (U16* buffer, size_t buffSize, double p, U32 seedSrc)
 
     /* Fill buffer */
     while (op<oend) {
-        const U32 r = FUZ_rand(&seed) & (PROBATABLESIZE-1);
-        *op++ = tableU16[r];
+        const U32 v16 = FUZ_rand(&seed) & (PROBATABLESIZE-1);
+        *op++ = tableU16[v16];
     }
 }
 
 
-#define CHECK(cond, ...) if (cond) { DISPLAY("Error => "); DISPLAY(__VA_ARGS__); \
-                         DISPLAY(" (seed %u, test nb %u)  \n", startSeed, testNb); exit(-1); }
+#define CHECK(cond, ...)                                          \
+    if (cond) {                                                   \
+        DISPLAY("Error => ");                                     \
+        DISPLAY(__VA_ARGS__);                                     \
+        DISPLAY(" (seed %u, test nb %u)  \n", startSeed, testNb); \
+        exit(-1);                                                 \
+}
 
 static void FUZ_tests (const U32 startSeed, U32 totalTest, U32 startTestNb)
 {
-    size_t bufferDstSize = BUFFERSIZE*sizeof(U16) + 64;
-    U16* bufferP8    = (U16*) malloc (bufferDstSize);
-    void* bufferDst  =        malloc (bufferDstSize);
-    U16* bufferVerif = (U16*) malloc (bufferDstSize);
-    unsigned testNb;
+    size_t const bufferDstSize = BUFFERSIZE*sizeof(U16) + 64;
+    U16* const  bufferP8    = (U16*) malloc (bufferDstSize);
+    U16* const  bufferP80   = (U16*) malloc (bufferDstSize);
+    void* const bufferDst   =        malloc (bufferDstSize);
+    U16* const  bufferVerif = (U16*) malloc (bufferDstSize);
     const size_t maxTestSizeMask = 0x1FFFF;
     U32 time = FUZ_GetMilliStart();
     U32 seed = startSeed;
+    unsigned testNb;
 
-    generateU16 (bufferP8, BUFFERSIZE, 0.08, seed);
+    if (!bufferP8 || !bufferP80 || !bufferDst || !bufferVerif) {
+        DISPLAY("memory allocation error \n");
+        exit(1);
+    }
+    generateU16 (bufferP8,  BUFFERSIZE, 240, 0.08, seed);
+    generateU16 (bufferP80, BUFFERSIZE, 257, 0.80, seed+1);
 
-    if (startTestNb) {
-        U32 i;
-        for (i=0; i<startTestNb; i++)
+    if (startTestNb) {   /* sync random seed */
+        U32 u;
+        for (u=0; u<startTestNb; u++)
             FUZ_rand (&seed);
     }
 
     for (testNb=startTestNb; testNb<totalTest; testNb++) {
-        U16* bufferTest;
         int tag=0;
         U32 roundSeed = seed ^ 0xEDA5B371;
         FUZ_rand(&seed);
@@ -178,51 +189,72 @@ static void FUZ_tests (const U32 startSeed, U32 totalTest, U32 startTestNb)
         }
 
         /* Compression / Decompression tests */
-        {
-            size_t sizeOrig = (FUZ_rand (&roundSeed) & maxTestSizeMask) + 1;
-            size_t offset = (FUZ_rand(&roundSeed) % (BUFFERSIZE - 64 - maxTestSizeMask));
-            size_t sizeCompressed;
-            U64 hashOrig;
-            bufferTest = bufferP8 + offset;
+        {   size_t const sizeOrig = (FUZ_rand (&roundSeed) & maxTestSizeMask) + 1;
+            size_t const offset = (FUZ_rand(&roundSeed) % (BUFFERSIZE - 64 - maxTestSizeMask));
+            const U16* const bufferSrc = (FUZ_rand(&roundSeed) & 0x1FF) ? bufferP8 : bufferP80;
+            const U16* const bufferTest = bufferSrc + offset;
 
-            DISPLAYLEVEL (4,"\b\b\b\b%3i ", tag++);
-            hashOrig = XXH64 (bufferTest, sizeOrig * sizeof(U16), 0);
-            sizeCompressed = FSE_compressU16 (bufferDst, bufferDstSize, bufferTest, sizeOrig, FSE_MAX_SYMBOL_VALUE, 12);
+            U64 const hashOrig = XXH64 (bufferTest, sizeOrig * sizeof(U16), 0);
+            size_t const sizeCompressed = FSE_compressU16(
+                                                bufferDst, bufferDstSize,
+                                                bufferTest, sizeOrig,
+                                                FSE_MAX_SYMBOL_VALUE, 12);
             CHECK(FSE_isError(sizeCompressed), "\r test %5u : FSE_compressU16 failed !", testNb);
+            DISPLAYLEVEL (4,"\b\b\b\b%3i ", tag++);
+
             if (sizeCompressed > 1) {  /* don't check uncompressed & rle corner cases */
-                U64 hashEnd;
-                U16 saved = (bufferVerif[sizeOrig] = 1024 + 250);
-                size_t dstSize;
-                size_t result;
+                U16 const guardValue = 1024 + 250;
 
                 /* basic decompression test : should work */
                 DISPLAYLEVEL (4,"\b\b\b\b%3i ", tag++);
-                result = FSE_decompressU16 (bufferVerif, sizeOrig, bufferDst, sizeCompressed);
-                CHECK(bufferVerif[sizeOrig] != saved, "\r test %5u : FSE_decompressU16 overrun output buffer (write beyond specified end) !", testNb);
-                CHECK(FSE_isError(result), "\r test %5u : FSE_decompressU16 failed : %s ! (origSize = %u shorts, cSize = %u bytes)", testNb, FSE_getErrorName(result), (U32)sizeOrig, (U32)sizeCompressed);
-                hashEnd = XXH64 (bufferVerif, result * sizeof(U16), 0);
-                CHECK(hashEnd != hashOrig, "\r test %5u : Decompressed data corrupted !!", testNb);
+                bufferVerif[sizeOrig] = guardValue;
+                {   size_t const dSize = FSE_decompressU16 (bufferVerif, sizeOrig, bufferDst, sizeCompressed);
+                    CHECK(bufferVerif[sizeOrig] != guardValue,
+                        "\r test %5u : FSE_decompressU16 overrun output buffer (write beyond specified end) !",
+                        testNb);
+                    CHECK(FSE_isError(dSize),
+                        "\r test %5u : FSE_decompressU16 failed : %s ! (origSize = %u shorts, cSize = %u bytes)",
+                        testNb, FSE_getErrorName(dSize), (U32)sizeOrig, (U32)sizeCompressed);
+                    {   U64 const hashEnd = XXH64 (bufferVerif, dSize * sizeof(U16), 0);
+                        CHECK(hashEnd != hashOrig,
+                            "\r test %5u : Decompressed data corrupted !!",
+                            testNb);
+                }   }
 
                 /* larger output buffer than necessary : should work */
                 DISPLAYLEVEL (4,"\b\b\b\b%3i ", tag++);
-                result = FSE_decompressU16 (bufferVerif, sizeOrig + (FUZ_rand(&roundSeed) & 31) + 1, bufferDst, sizeCompressed);
-                CHECK(FSE_isError(result), "\r test %5u : FSE_decompressU16 failed : %s ! (origSize = %u shorts, cSize = %u bytes)", testNb, FSE_getErrorName(result), (U32)sizeOrig, (U32)sizeCompressed);
-                hashEnd = XXH64 (bufferVerif, result * sizeof(U16), 0);
-                CHECK(hashEnd != hashOrig, "\r test %5u : Decompressed data corrupted !!", testNb);
+                {   size_t const dSize = FSE_decompressU16(
+                        bufferVerif, sizeOrig + (FUZ_rand(&roundSeed) & 31) + 1,
+                        bufferDst, sizeCompressed);
+                    CHECK(FSE_isError(dSize),
+                        "\r test %5u : FSE_decompressU16 failed : %s ! (origSize = %u shorts, cSize = %u bytes)",
+                        testNb, FSE_getErrorName(dSize), (U32)sizeOrig, (U32)sizeCompressed);
+                    {   U64 const hashEnd = XXH64 (bufferVerif, dSize * sizeof(U16), 0);
+                        CHECK(hashEnd != hashOrig,
+                            "\r test %5u : Decompressed data corrupted !!",
+                            testNb);
+                }   }
 
                 /* smaller output buffer than required : should fail */
                 DISPLAYLEVEL (4,"\b\b\b\b%3i ", tag++);
-                dstSize = (FUZ_rand(&roundSeed) & 31) + 1;
-                if (dstSize >= sizeOrig) dstSize = 1;
-                dstSize = sizeOrig - dstSize;
-                saved = (bufferVerif[dstSize] = 1024 + 250);
-                result = FSE_decompressU16 (bufferVerif, dstSize, bufferDst, sizeCompressed);
-                CHECK(bufferVerif[dstSize] != saved, "\r test %5u : FSE_decompressU16 overrun output buffer (write beyond specified end) !", testNb);
-                CHECK(!FSE_isError(result), "\r test %5u : FSE_decompressU16 should have failed ! (origSize = %u shorts, dstSize = %u bytes)", testNb, (U32)sizeOrig, (U32)dstSize);
-    }   }   }
+                {   size_t const missing = (FUZ_rand(&roundSeed) & 31) + 1;
+                    size_t const missing_fixed = (missing>=sizeOrig) ? 1 : missing;
+                    size_t const dstSize = sizeOrig - missing_fixed;
+                    bufferVerif[dstSize] = guardValue;
+                    {   size_t const dSize = FSE_decompressU16(
+                                                    bufferVerif, dstSize,
+                                                    bufferDst, sizeCompressed);
+                        CHECK(bufferVerif[dstSize] != guardValue,
+                            "\r test %5u : FSE_decompressU16 overrun output buffer (write beyond specified end) !",
+                            testNb);
+                        CHECK(!FSE_isError(dSize),
+                            "\r test %5u : FSE_decompressU16 should have failed ! (origSize = %u shorts, dstSize = %u bytes)",
+                            testNb, (U32)sizeOrig, (U32)dstSize);
+    }   }   }   }   }
 
     /* clean */
     free (bufferP8);
+    free (bufferP80);
     free (bufferDst);
     free (bufferVerif);
 }
@@ -232,30 +264,30 @@ static void FUZ_tests (const U32 startSeed, U32 totalTest, U32 startTestNb)
 *  Unitary tests
 *****************************************************************/
 
-extern size_t FSE_countU16(unsigned* count, unsigned* maxSymbolValuePtr, const unsigned short* source, size_t sourceSize);
+extern size_t FSE_countU16(unsigned* count, unsigned* maxSymbolValuePtr,
+               const unsigned short* source, size_t sourceSize);
 
 #define TBSIZE (16 KB)
 static void unitTest(void)
 {
-    U16  testBuffU16[TBSIZE];
-    size_t errorCode;
-    U32 startSeed=0, testNb=0;
+    U16 testBuffU16[TBSIZE];
+    U32 startSeed=0, testNb=0;   /* just to re-use CHECK */
 
     /* FSE_countU16 */
-    {
-        U32 table[FSE_MAX_SYMBOL_VALUE+2];
-        U32 max, i;
+    U32 table[FSE_MAX_SYMBOL_VALUE+2];
+    U32 u;
 
-        for (i=0; i< TBSIZE; i++) testBuffU16[i] = i % (FSE_MAX_SYMBOL_VALUE+1);
+    for (u=0; u< TBSIZE; u++)
+        testBuffU16[u] = u % (FSE_MAX_SYMBOL_VALUE+1);
 
-        max = FSE_MAX_SYMBOL_VALUE;
-        errorCode = FSE_countU16(table, &max, testBuffU16, TBSIZE);
-        CHECK(FSE_isError(errorCode), "FSE_countU16() should have worked");
+    { U32 max = FSE_MAX_SYMBOL_VALUE;
+      size_t const errC = FSE_countU16(table, &max, testBuffU16, TBSIZE);
+      CHECK(FSE_isError(errC), "FSE_countU16() should have worked"); }
 
-        max = FSE_MAX_SYMBOL_VALUE-1;
-        errorCode = FSE_countU16(table, &max, testBuffU16, TBSIZE);
-        CHECK(!FSE_isError(errorCode), "FSE_countU16() should have failed : max too low");
-    }
+    { U32 max = FSE_MAX_SYMBOL_VALUE-1;
+      size_t const errC = FSE_countU16(table, &max, testBuffU16, TBSIZE);
+      CHECK(!FSE_isError(errC),
+            "FSE_countU16() should have failed : max too low"); }
 
     DISPLAY("Unit tests completed\n");
 }
@@ -264,19 +296,19 @@ static void unitTest(void)
 /*****************************************************************
 *  Command line
 *****************************************************************/
-int main (int argc, char** argv)
+int main (int argc, const char** argv)
 {
-    U32 seed, startTestNb=0, pause=0, totalTest = FUZ_NB_TESTS;
+    U32 startTestNb=0, pause=0, totalTest = FUZ_NB_TESTS;
     int argNb;
 
-    seed = FUZ_GetMilliStart() % 10000;
-    DISPLAYLEVEL (1, "FSE U16 (%2i bits) automated test\n", (int)sizeof(void*)*8);
+    U32 seed = FUZ_GetMilliStart() % 10000;
+    DISPLAYLEVEL(1, "FSE U16 (%2i bits) automated test\n",(int)sizeof(void*)*8);
     for (argNb=1; argNb<argc; argNb++) {
-        char* argument = argv[argNb];
+        const char* argument = argv[argNb];
         if (argument[0]=='-') {
             argument++;
-            while (argument[0]!=0) {
-                switch (argument[0])
+            while (*argument!=0) {
+                switch (*argument)
                 {
                 /* seed setting */
                 case 's':
